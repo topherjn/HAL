c59ee1e3b59c1e4dafee077d41f97934300312a5
Christopher Nicholson <nicholsonc@gmail.com>
2017-04-13 18:06:59 -0400

    Break up dictionary word into array of strings

--- HAL/HAL.cs
+++ HAL/HAL.cs
@@ -12,6 +12,26 @@ namespace HAL
             // import the entire dictionary into one string
             string dict = Properties.Resources.dict;
 
+            // break up into an array of strings
+            char[] delimiters = new char[] { '\r', '\n' };
+            string[] lines = dict.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            // insert each word into a binary tree
+            foreach(var word in lines)
+            {
+                Console.WriteLine(word);
+            }
+
+            // take each word
+
+            // shift each letter in the word by a number
+
+            // see if there is a hit in the dictionary
+
+            // if yes add to a list of hits
+
+
+
             // the followig console write demonstrates the success of the resource
             // import.  You will want to comment it out or delete it eventually
             Console.WriteLine(dict);
