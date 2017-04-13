bb8e6f360eb7c00dc1b1f38e7169d6eba71385ec
Christopher Nicholson <nicholsonc@gmail.com>
2017-04-12 20:50:05 -0400

    Initial commit

---
+++ HAL/HAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+
+namespace HAL
+{
+    class HAL
+    {
+        static void Main(string[] args)
+        {
+            // import the entire dictionary into one string
+            string dict = Properties.Resources.dict;
+
+            // the followig console write demonstrates the success of the resource
+            // import.  You will want to comment it out or delete it eventually
+            Console.WriteLine(dict);
+
+        }
+
+    }
+}
+
