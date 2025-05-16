90e9825a6af8644e0409d7a3c072df88664f4f6b
topherjn <nicholsonc@gmail.com>
2025-05-16 16:29:47 -0400

    Added more stats about longest word and shift values

--- HAL/HAL.cs
+++ HAL/HAL.cs
@@ -13,12 +13,11 @@ namespace HAL
             // Load the dictionary from the embedded resource
             // Note: The dictionary should be a text file with one word per line
             string dict = Properties.Resources.dict;
-            // Split the dictionary into words and store them in a HashSet for fast lookup
-            HashSet<string> words = new HashSet<string>(dict.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries));
-            HashSet<(string, string)> uniquePairs = new HashSet<(string, string)>();
-            
             int max = 0;
             string longestWord = string.Empty;
+            // Split the dictionary into words and store them in a HashSet for fast lookup
+            HashSet<string> words = new HashSet<string>(dict.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries));
+            HashSet<(string, string, int, int)> uniquePairs = new HashSet<(string, string, int, int)>();
 
             // Iterate through each possible shift (1 to 25)
             for (int j = 1; j < 26; j++)
@@ -30,6 +29,7 @@ namespace HAL
                     foreach (char c in word)
                     {
                         if (char.IsLetter(c))
+                            // Shift the character by j positions in the alphabet
                             shifted.Append((char)((c - 'a' + j) % 26 + 'a'));
                         else
                             shifted.Append(c);
@@ -39,9 +39,11 @@ namespace HAL
 
                     if (words.Contains(shiftedWord))
                     {
-                        // Store word pairs in a consistent order
-                        var orderedPair = word.CompareTo(shiftedWord) < 0 ? (word, shiftedWord) : (shiftedWord, word);
-                        uniquePairs.Add(orderedPair);
+                        // Store word pairs in a consistent order, along with both shift directions
+                        if (word.CompareTo(shiftedWord) < 0)
+                            uniquePairs.Add((word, shiftedWord, j, 26 - j));
+                        else
+                            uniquePairs.Add((shiftedWord, word, 26 - j, j));
 
                         if (shiftedWord.Length > max)
                         {
@@ -52,13 +54,16 @@ namespace HAL
                 }
             }
 
-            // Print the longest shifted word found
-            Console.WriteLine($"Longest shifted word: {longestWord} with length {max}");
+            var longestPair = uniquePairs.FirstOrDefault(pair => pair.Item2 == longestWord || pair.Item1 == longestWord);
+            if (longestPair != default)
+            {
+                Console.WriteLine($"Longest word pair: {longestPair.Item1} - {longestPair.Item2} | Shift: {longestPair.Item3} forward, {longestPair.Item4} backward");
+            }
 
-            // Print unique word pairs found
+            // Print unique word pairs with shift values
             foreach (var pair in uniquePairs)
             {
-                Console.WriteLine($"{pair.Item1} - {pair.Item2}");
+                Console.WriteLine($"{pair.Item1} - {pair.Item2} | Shift: {pair.Item3} forward, {pair.Item4} backward");
             }
         }
     }
